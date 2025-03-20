3d343eb8facd8692c8cc3ddd5e5e7b7c5dc1fd12
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-03-20 13:45:17 +1300

    Add files via upload

---
+++ 3numbers_swapped.py
@@ -0,0 +1,18 @@
+# incorrect number variables:
+incorrect_num = 12
+incorrect_num2 = 9
+incorrect_num3 = 15
+print(f"{incorrect_num},{incorrect_num2},{incorrect_num3} are the 3 incorrect numbers")
+# incorrect maths question:
+inc_sum = (incorrect_num * incorrect_num2 )//incorrect_num3
+#incorrect sum
+print(f"{inc_sum} is the incorrect sum")
+# correct number variables:
+correct_num = 12
+correct_num2 = 6
+correct_num3 = 25
+print(f"{correct_num},{correct_num2},{correct_num3} are the 3 correct numbers")
+# correct maths question:
+c_sum = (correct_num*correct_num2)//correct_num3
+# correct sum:
+print(f"{c_sum}, is the correct sum")
